97de8df1449ba93421de3cfb96e74ebc467c8015
Saphle <kitakazeaya@gmail.com>
2018-10-23 17:44:28 +0200

    What's this? Commands have CONTEXT now? Whoo, less GameState-checking!

--- src/main/java/MazeOfMaws/CommandExecutor.java
+++ src/main/java/MazeOfMaws/CommandExecutor.java
@@ -1,7 +1,7 @@
 package MazeOfMaws;
 
+import Game.GameState;
 import Game.Player;
-import MazeOfMaws.Commands.Command;
 import MazeOfMaws.Commands.*;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
@@ -9,18 +9,22 @@ import java.util.HashMap;
 
 class CommandExecutor {
 
-    private HashMap<String, Command> commandMap;
+    private HashMap<String, Command> menuMap;
+    private HashMap<String, Command> gameMap;
 
     CommandExecutor() {
         initCommands();
     }
 
     private void initCommands() {
-        commandMap = new HashMap<>();
-        commandMap.put("welcome", new Welcome());
-        commandMap.put("status", new SendStatus());
-        commandMap.put("start", new StartGame());
-        commandMap.put("end", new EndGame());
+        menuMap = new HashMap<>();
+        menuMap.put("welcome", new Welcome());
+        menuMap.put("start", new StartGame());
+        menuMap.put("status", new SendStatus());
+
+        gameMap.put("status", new SendStatus());
+        gameMap.put("end", new EndGame());
+        gameMap.put("look", new Look());
     }
 
     String sendWelcomeMessage(MessageReceivedEvent event) {
@@ -28,6 +32,12 @@ class CommandExecutor {
     }
 
     String executeCommand(Player player, String command, String args) {
-        return commandMap.get(command).run(player, args);
+        if (player == null) {
+            return "This error should never occur. Something has gone very, very wrong.";
+        }
+        if (player.getState() == GameState.NOT_STARTED) {
+            return menuMap.get(command).run(player, args);
+        }
+        return gameMap.get(command).run(player, args);
     }
 }
